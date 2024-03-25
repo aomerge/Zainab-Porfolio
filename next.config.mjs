5bc85a79909b065af2f1058ca03db8b6b6f24359
Ángel Ortega <codenode.ortega@gmail.com>
2024-03-25 04:43:28 -0600

    Update Next.js build configuration and config export static next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  assetPrefix: "https://github.com/aomerge/Zainab-Porfolio",
+};
 
 export default nextConfig;
