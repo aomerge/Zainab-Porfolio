87b77a09584b5dd6aa8b09b4215385c286c210a6
Ángel Ortega <codenode.ortega@gmail.com>
2024-04-08 21:57:27 -0600

    Refactor Next.js configuration and GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
+// next.config.mjs
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  // No necesitas especificar `output: 'export'` aquí.
-  // Asegúrate de usar `next build && next export` en tus scripts.
   basePath: isProd ? '/Zainab-Porfolio' : '',
   assetPrefix: isProd ? '/Zainab-Porfolio/' : '',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
 
